be32449d12d80e04feeec29292709406aabd104a
fariasdavi1999 <you@example.com>
2022-08-10 20:41:07 -0300

    Inserindo anotações CrossOrign

--- src/main/java/com/megatec/backendmegatec/controller/ProdutoController.java
+++ src/main/java/com/megatec/backendmegatec/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,30 +35,36 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    // ao inves de "*" colocar o endpoint (url)
+    @CrossOrigin(origins = "https://megatec-store.herokuapp.com/")
     @GetMapping
     public List<Produto> obterTodos() {
 
         return produtoService.obterTodos();
     }
 
+    @CrossOrigin(origins = "https://megatec-store.herokuapp.com/")
     @GetMapping("/{id}")
     public Optional<Produto> obterPorId(@PathVariable Integer id) {
 
         return produtoService.obterPorId(id);
     }
 
+    @CrossOrigin(origins = "https://megatec-store.herokuapp.com/")
     @PostMapping
     public Produto adicionar(@RequestBody Produto produto) {
 
         return produtoService.adicionar(produto);
     }
 
+    @CrossOrigin(origins = "https://megatec-store.herokuapp.com/")
     @DeleteMapping("/{id}")
     public String deletar(@PathVariable Integer id) {
         produtoService.deletar(id);
         return "Produto com id: " + id + " Deletado com Sucesso";
     }
 
+    @CrossOrigin(origins = "https://megatec-store.herokuapp.com/")
     @PutMapping("/{id}")
     public Produto alterar(@RequestBody Produto produto, @PathVariable Integer id) {
         return produtoService.alterar(id, produto);
